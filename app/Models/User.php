d1d541de2a4a4b8d630629e3b86cf00b34388ebc
devam-joshi <devamjoshi8@gmail.com>
2026-03-12 15:04:27 +0530

    Updated Code

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,8 @@ class User extends Authenticatable
         'password',
         'contact',
         'status',
-        'profile_image'
+        'profile_image',
+        'role_id'
     ];
 
     /**
